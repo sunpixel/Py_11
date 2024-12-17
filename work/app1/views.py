e672b0d42a3f05ad9611cafb9f40362c452a1432
SunPixel <tarakinnikita@gmail.com>
2024-12-17 17:26:43 +0300

    Added Templates + views and urls

---
+++ work/app1/views.py
@@ -0,0 +1,31 @@
+'''Views file for app1'''
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    '''Returns main page'''
+    return render(request, 'index.html')
+
+def about(request):
+    '''Returns about page'''
+    return render(request, 'about.html')
+
+def contact(request):
+    '''Returns contact page'''
+    return render(request, 'contact.html')
+
+def data_list(request):
+    '''Returns data list page'''
+    data = ['Apple', 'Banana', 'Corn', 'Veggies', 'Fruits']
+    return render(request, 'data.html', {'data': data})
+
+def render_data(request):
+    '''Returns data list page'''
+    data = [
+        {'id': 1, 'name': 'John Doe', 'age': 28, 'city': 'Moscow'},
+        {'id': 2, 'name': 'Jane Smith', 'age': 34, 'city': 'Volgograd'},
+        {'id': 3, 'name': 'Mike Johnson', 'age': 45, 'city': 'Chicago'},
+        {'id': 4, 'name': 'Emily Davis', 'age': 22, 'city': 'London'},
+    ]
+    return render(request, 'render.html', {'data': data})
---
+++ work/work/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for work project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include, re_path
+from app1 import views
+
+app1_patters = [
+    path('', views.index),
+    path('about/', views.about),
+    path('contact/', views.contact),
+    path('data/', views.data_list),
+    path('render/', views.render_data),
+]
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'^app1/', include(app1_patters)),
+]
